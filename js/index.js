5b497b2fb3d5532e0d648bb66805a519cab2962c
TeppieC <teppie.c@hotmail.com>
2016-07-10 18:55:58 +0800

    partially finished slides

--- js/index.js
+++ js/index.js
@@ -1,12 +1,93 @@
-var noTipButton = document.getElementsByClassName('no-tips')[0];
+function getCookie(Name) {
+    var cookie = {};
+    var all = document.cookie;
+    if (all === '')
+        return null;
+    var list = all.split(';');
+    for (var i = 0; i < list.length; i++) {
+        var item = list[i];
+        var p = item.indexOf('=');
+        var name = item.substring(0, p);
+        name = decodeURIComponent(name);
+        var value = item.substring(p + 1);
+        value = decodeURIComponent(value);
+        cookie[name] = value;
+    }
+    return cookie[Name];
+}
 
-noTipButton.addEventListener('click', 
-	function(event) {
-		var tipBar = document.getElementsByClassName("g-tips")[0];
-		tipBar.className+=' f-hide';
-	}, false);
+function setCookie(name, value, expires, path, domain, secure) {
+    var cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+    if (expires)
+        cookie += ';expires=' + expires.toGMTString();
+    if (path)
+        cookie += ';path=' + path;
+    if (domain)
+        cookie += ';domain=' + domain;
+    if (secure)
+        cookie += ';secure=' + secure;
+    document.cookie = cookie;
+}
 
+function removeCookie(name, path, domain) {
+    setCookie(name, '', new Date(0), path, domain);
+}
 
+var closeNotice = function(){
+	if(getCookie('hide-notice')=='true'){
+		var noticeBar = document.getElementsByClassName("g-tips")[0];
+		noticeBar.className+=' f-hide';
+		return;}			
+	var noticeBar = document.getElementsByClassName("g-tips")[0];
+	var noTipButton = document.getElementsByClassName('no-tips')[0];
+	noTipButton.addEventListener('click', 
+		function(event) {
+			noticeBar.className+=' f-hide';
+			setCookie('hide-notice','true',new Date())}, false);}();
+
+function changePage(){
+	var controls = document.querySelector(".g-banner .m-banner .m-controls").childNodes;
+	console.log(controls.length);
+	for (var i=0;i<controls.length;i++){
+		//controls[i].addEventListener('click', handler, false);
+		addClickHandler(controls[i],i);
+	}
+
+	function addClickHandler(elem, index) {
+    	elem.addEventListener('click', function(e) {
+			var current = document.querySelector(".g-banner .m-banner .m-controls .cur");
+			var clicked = controls[index];
+			current.className = "";
+			clicked.className = "cur";
+        }, false);
+	}
+
+	/*
+	var handler = function(event, index){
+		var current = document.querySelector(".g-banner .m-banner .m-controls .cur");
+		var clicked = controls[index];
+		current.className = "";
+		clicked.className = "cur";
+	}*/
+}
+changePage();
+
+function slide(){
+	var current = document.getElementsByClassName('current')[0];
+	var next = current.nextSibling;
+	current.className = 'banner-picture';
+	//console.log(next.nodeName);
+	if (next==null){
+		next = document.querySelectorAll('.m-banner .banners img')[0];
+	}
+	next.className += " current";
+}
+
+var autoSlide = function(){timer=setInterval(slide,5000);}();
+
+//find the highest z-index
+//set to invisible
+//find its sibling
 /*
 (window.onload = function(){
 	function getElementsByClassName(root, className) {
